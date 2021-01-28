a7997566b3cae516f722c59a15b86f21fd674c83
Pavel Pigal <pavel.pigal@gmail.com>
2021-01-28 08:52:35 +0300

    powershell script

---
+++ 16_2.ps1
@@ -0,0 +1,37 @@
+﻿function Remove-Filesgt {
+    [CmdletBinding()]
+      <#
+    .SYNOPSIS
+        This function remove all files greater then Number
+    .DESCRIPTION
+        Long description
+    .EXAMPLE
+        PS C:\> get-Filegt
+        Explanation of what the example does
+    .INPUTS
+        Inputs (if any)
+    .OUTPUTS
+        Output (if any)
+    .NOTES
+        General notes
+    #>
+    param (
+
+        [string]$Path=".\",
+        [int]$Number = 8192
+    )
+    
+    begin {
+        
+    }
+    
+    process {#Get-ChildItem -path $Path -af|Select-Object Name,Length| Sort-Object -descending -property length
+             Get-ChildItem -Path $path  -Force | Where-Object { $_.Length -gt $Number } | Remove-Item -Force
+        
+    }
+    
+    end {
+        
+        
+    }
+}
